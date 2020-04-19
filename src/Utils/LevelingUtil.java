358791f168f635f6834c1f5218a74ee0a3bacaaf
Houska02 <SpigotDevTec@email.cz>
2020-04-19 19:58:35 +0200

    new leveling methods

--- src/Utils/LevelingUtil.java
+++ src/Utils/LevelingUtil.java
@@ -14,16 +14,21 @@ public class LevelingUtil {
 	 public static int getToolLevel(Player p) {
 		 int level = 0;
 		 @SuppressWarnings("deprecation")
-		ItemStack item = p.getItemInHand();
-			ItemMeta m = item.getItemMeta();
+		 ItemStack item = p.getItemInHand();
+		 ItemMeta m = item.getItemMeta();
 		 List<String> lore = m.getLore();
 		 
-		 if(lore!=null)
-				for(String ss:lore) {
-					if(ss.startsWith("Item level")) {
-						level = TheAPI.getStringUtils().getInt(ss.replace("Item level ", ""));
-					}
+		 if(lore!=null) {
+			 for(String ss:lore) {
+				if(ss.startsWith("Item level")) {
+					level = TheAPI.getStringUtils().getInt(ss.replace("Item level ", ""));
+					break;
 				}
+				level = 0;
+			}	
+		 }else {
+			 level=0;
+			 }
 		 return level;
 	    }
 	public static int getToolXP(Player p) {
@@ -33,18 +38,24 @@ public class LevelingUtil {
 			ItemMeta m = item.getItemMeta();
 		 List<String> lore = m.getLore();
 		 
-		 if(lore!=null)
+		 if(lore!=null) {
 				for(String ss:lore) {
 					if(ss.startsWith("Xp for next level")) {
 						xp = TheAPI.getStringUtils().getInt(ss.replace("Xp for next level ", ""));
+						break;
 					}
+					xp = 0;
 				}
+		 }else {
+			 xp = 0;
+		 }
 		 return xp;
 	    }
+	
 	 static void setlevel(Player p, int level) {
 		 int line = 0;
-		 int finalline = 0;
-		 int finalline2 = 0;
+		 int levelLine = 0;
+		 int xpLine = 0;
 		 @SuppressWarnings("deprecation")
 			ItemStack item = p.getItemInHand();
 				ItemMeta m = item.getItemMeta();
@@ -53,17 +64,17 @@ public class LevelingUtil {
 				 if(lore!=null)
 						for(String ss:lore) {
 							if(ss.startsWith("Item level")) {
-								finalline2 = line;
+								levelLine = line;
 							}
 							if(ss.startsWith("Xp for next level")) {
-								finalline = line;
+								xpLine = line;
 							}
 							line=line+1;
 						}
-				lore.remove(finalline);
-				lore.remove(finalline2);
-				lore.add("Item level"+level);
-				lore.add("Xp for next level"+level);
+				lore.remove(levelLine);
+				lore.remove(xpLine);
+				lore.add("Item level "+level);
+				lore.add("Xp for next level "+getToolXP(p));
 				m.setLore(lore);
 				item.setItemMeta(m);
 	 }
@@ -88,9 +99,32 @@ public class LevelingUtil {
 						}
 				lore.remove(finalline);
 				lore.remove(finalline2);
-				lore.add("Item level"+getToolLevel(p));
-				lore.add("Xp for next level"+level);
+				lore.add("Item level "+getToolLevel(p));
+				lore.add("Xp for next level "+level);
 				m.setLore(lore);
 				item.setItemMeta(m);
+	 }
+	 static void addXp(Player p, int xpammount) {
+		 int level = getToolLevel(p);
+		 int xp = getToolXP(p);
+		 int neededxp = 0;
+		 if(level<=50) neededxp = 10*level;
+		 if(level>50&&level<=100) neededxp = 15*level;
+		 if(level>100&&level<=150) neededxp = 20*level;
+		 if(level>150&&level<=200) neededxp = 25*level;
+		 if(level>300&&level<=400) neededxp = 35*level;
+		 if(level>400&&level<=500) neededxp = 45*level;
+		 if(level>500) neededxp = 55*level;
+			 if(level==0) {
+				 setlevel(p, 1);
+			 }
+			 if(xp>neededxp) {
+				 setXP(p, xpammount);
+				 return;
+			 }
+			 if(xp==neededxp) {
+				 setlevel(p, level+1);
+				 return;
+			 }
 	 }
 }
