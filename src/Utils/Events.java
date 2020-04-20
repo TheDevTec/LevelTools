49a8ed248c7892c3647d0f4a8f102b9c1ecfe136
Rajskoo <unconfigured@null.spigotmc.org>
2020-04-20 17:41:04 +0200

    Added tools, to utils.

--- src/Utils/Events.java
+++ src/Utils/Events.java
@@ -8,33 +8,75 @@ import org.bukkit.event.block.BlockBreakEvent;
 
 public class Events implements Listener {
 
-	@SuppressWarnings("unlikely-arg-type")
 	public void onBreak(BlockBreakEvent e) {
 		Block block = e.getBlock();
 		Material material = block.getType();
 		Player p = e.getPlayer();
 		
 		if (p.getInventory().getItemInMainHand().getType() == Material.WOODEN_SHOVEL) {
-			if (material.equals(block)) {
+			if (material.equals(block.getType())) {
 				Utils.LevelingUtil.getToolXP(p);
 			}
 		} else if (p.getInventory().getItemInMainHand().getType() == Material.STONE_SHOVEL) {
-			if (material.equals(block)) {
+			if (material.equals(block.getType())) {
 				Utils.LevelingUtil.getToolXP(p);
 			}
 		} else if (p.getInventory().getItemInMainHand().getType() == Material.IRON_SHOVEL) {
-			if (material.equals(block)) {
+			if (material.equals(block.getType())) {
 				Utils.LevelingUtil.getToolXP(p);
 			}
 		} else if (p.getInventory().getItemInMainHand().getType() == Material.GOLDEN_SHOVEL) {
-			if (material.equals(block)) {
+			if (material.equals(block.getType())) {
 				Utils.LevelingUtil.getToolXP(p);
 			}
 		} else if (p.getInventory().getItemInMainHand().getType() == Material.DIAMOND_SHOVEL) {
-			if (material.equals(block)) {
+			if (material.equals(block.getType())) {
+				Utils.LevelingUtil.getToolXP(p);
+			}
+		}
+		
+		if (p.getInventory().getItemInMainHand().getType() == Material.WOODEN_PICKAXE) {
+			if (material.equals(block.getType())) {
+				Utils.LevelingUtil.getToolXP(p);
+			}
+		} else if (p.getInventory().getItemInMainHand().getType() == Material.STONE_PICKAXE) {
+			if (material.equals(block.getType())) {
+				Utils.LevelingUtil.getToolXP(p);
+			}
+		} else if (p.getInventory().getItemInMainHand().getType() == Material.IRON_PICKAXE) {
+			if (material.equals(block.getType())) {
+				Utils.LevelingUtil.getToolXP(p);
+			}
+		} else if (p.getInventory().getItemInMainHand().getType() == Material.GOLDEN_PICKAXE) {
+			if (material.equals(block.getType())) {
+				Utils.LevelingUtil.getToolXP(p);
+			}
+		} else if (p.getInventory().getItemInMainHand().getType() == Material.DIAMOND_PICKAXE) {
+			if (material.equals(block.getType())) {
+				Utils.LevelingUtil.getToolXP(p);
+			}
+		}
+		
+		if (p.getInventory().getItemInMainHand().getType() == Material.WOODEN_AXE) {
+			if (material.equals(block.getType())) {
+				Utils.LevelingUtil.getToolXP(p);
+			}
+		} else if (p.getInventory().getItemInMainHand().getType() == Material.STONE_AXE) {
+			if (material.equals(block.getType())) {
+				Utils.LevelingUtil.getToolXP(p);
+			}
+		} else if (p.getInventory().getItemInMainHand().getType() == Material.IRON_AXE) {
+			if (material.equals(block.getType())) {
+				Utils.LevelingUtil.getToolXP(p);
+			}
+		} else if (p.getInventory().getItemInMainHand().getType() == Material.GOLDEN_AXE) {
+			if (material.equals(block.getType())) {
+				Utils.LevelingUtil.getToolXP(p);
+			}
+		} else if (p.getInventory().getItemInMainHand().getType() == Material.DIAMOND_AXE) {
+			if (material.equals(block.getType())) {
 				Utils.LevelingUtil.getToolXP(p);
 			}
 		}
 	}
-	
 }
